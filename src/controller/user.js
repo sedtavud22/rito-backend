c5a285df7c9b0c1124335200fb4d4805c988ab2d
Your Name <112855044+SexyCornman@users.noreply.github.com>
2024-03-04 00:46:27 +0700

    added display name when register

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,97 +1,110 @@
-const repo = require("../repository")
-const utils = require("../utils")
-const { CustomError } = require("../config/error")
-const { Role } = require("@prisma/client")
+const repo = require("../repository");
+const utils = require("../utils");
+const { CustomError } = require("../config/error");
+const { Role } = require("@prisma/client");
 
 module.exports.getAll = async (req, res, next) => {
-    try {
-        const users = await repo.user.getAll()
-        res.status(200).json({ users })
-    } catch (err) {
-        next(err)
-    }
-    return
-}
+  try {
+    const users = await repo.user.getAll();
+    res.status(200).json({ users });
+  } catch (err) {
+    next(err);
+  }
+  return;
+};
 
-module.exports.getMe = async (req,res,next) =>{
-    try{
-        res.status(200).json({user: req.user})
-    }catch(err){
-        next(err)
-    }
-}
+module.exports.getMe = async (req, res, next) => {
+  try {
+    res.status(200).json({ user: req.user });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports.get = async (req, res, next) => {
-    try {
-        const { id } = req.params
-        const user = await repo.user.get({ id })
-        res.status(200).json({ user })
-    } catch (err) {
-        next(err)
-    }
-    return
-}
+  try {
+    const { id } = req.params;
+    const user = await repo.user.get({ id });
+    res.status(200).json({ user });
+  } catch (err) {
+    next(err);
+  }
+  return;
+};
 
 module.exports.login = async (req, res, next) => {
-    try {
-        const { username, email, password } = req.body
-        // GET username from database
-        const user = await repo.user.get({ OR: [{ username }, { email }] })
-        if (!user) throw new CustomError("invalid credentials", "WRONG_INPUT", 400)
+  try {
+    const { username, email, password } = req.body;
+    // GET username from database
+    const user = await repo.user.get({ OR: [{ username }, { email }] });
+    if (!user) throw new CustomError("invalid credentials", "WRONG_INPUT", 400);
 
-        // COMPARE password with database
-        const result = await utils.bcrypt.compare(password, user.password)
-        if (!result) throw new CustomError("invalid credentials", "WRONG_INPUT", 400)
+    // COMPARE password with database
+    const result = await utils.bcrypt.compare(password, user.password);
+    if (!result)
+      throw new CustomError("invalid credentials", "WRONG_INPUT", 400);
 
-        // DELETE KEY of password from user data
-        delete user.password
-        // SIGN token from user data
-        const token = utils.jwt.sign(user)
-        res.status(200).json({ token })
-    } catch (err) {
-        next(err)
-    }
-    return
-}
+    // DELETE KEY of password from user data
+    delete user.password;
+    // SIGN token from user data
+    const token = utils.jwt.sign(user);
+    res.status(200).json({ token });
+  } catch (err) {
+    next(err);
+  }
+  return;
+};
 module.exports.register = async (req, res, next) => {
-    try {
-        const { username, password, firstName, lastName,email, isAdmin } = req.body
-        // let role = Role.USER
-        // if (req.body.role != Role.ADMIN) role = Role.ADMIN
-        // HASHED PASSWORD
-        const hashed = await utils.bcrypt.hashed(password)
-        // CREATE user to database
-        const user = await repo.user.create({ username, password: hashed, firstName,lastName, email, isAdmin })
-        // DELETE KEY of password from user data
-        delete user.password
-        // SIGN token from user data
-        const token = utils.jwt.sign(user)
+  try {
+    const { username, password, firstName, lastName, email, isAdmin } =
+      req.body;
+    // let role = Role.USER
+    // if (req.body.role != Role.ADMIN) role = Role.ADMIN
+    // HASHED PASSWORD
+    const hashed = await utils.bcrypt.hashed(password);
+    // CREATE user to database
+    const user = await repo.user.create({
+      username,
+      displayName: username,
+      password: hashed,
+      firstName,
+      lastName,
+      email,
+      isAdmin,
+    });
+    // DELETE KEY of password from user data
+    delete user.password;
+    // SIGN token from user data
+    const token = utils.jwt.sign(user);
 
-        res.status(200).json({ token })
-    } catch (err) {
-        next(err)
-    }
-    return
-}
+    res.status(200).json({ token });
+  } catch (err) {
+    next(err);
+  }
+  return;
+};
 module.exports.update = async (req, res, next) => {
-    try {
-        const { id } = req.params
-        const { firstName, lastName, profileImageUrl } = req.body
-        const user = await repo.user.update({ id: +id }, { firstName, lastName,profileImageUrl })
+  try {
+    const { id } = req.params;
+    const { firstName, lastName, profileImageUrl } = req.body;
+    const user = await repo.user.update(
+      { id: +id },
+      { firstName, lastName, profileImageUrl }
+    );
 
-        res.status(200).json({ user })
-    } catch (err) {
-        next(err)
-    }
-    return
-}
+    res.status(200).json({ user });
+  } catch (err) {
+    next(err);
+  }
+  return;
+};
 module.exports.delete = async (req, res, next) => {
-    try {
-        const { id } = req.params
-        await repo.user.delete({ id:+id })
-        res.status(200).json({message:`user id:${id} deleted`})
-    } catch (err) {
-        next(err)
-    }
-    return
-}
+  try {
+    const { id } = req.params;
+    await repo.user.delete({ id: +id });
+    res.status(200).json({ message: `user id:${id} deleted` });
+  } catch (err) {
+    next(err);
+  }
+  return;
+};
