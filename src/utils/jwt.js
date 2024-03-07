0dd6956248c7f1a981b65588aa68d27eecf38e8f
sedtavud22 <phoosed@gmail.com>
2024-03-08 02:18:44 +0700

    forgot and reset password api

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,16 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 module.exports.sign = (payload) =>
-    jwt.sign(payload, process.env.JWT_SECRET_KEY ?? "key", {
-        algorithm: process.env.JWT_ALGORITHM ?? "HS384",
-        expiresIn: process.env.JWT_EXPIRES ?? "1d",
-    })
+  jwt.sign(payload, process.env.JWT_SECRET_KEY ?? "key", {
+    algorithm: process.env.JWT_ALGORITHM ?? "HS384",
+    expiresIn: process.env.JWT_EXPIRES ?? "1d",
+  });
+
+module.exports.signResetPassword = (payload) =>
+  jwt.sign(payload, process.env.JWT_RESET_PASSWORD_KEY ?? "key", {
+    expiresIn: process.env.JWT_RESET_PASSWORD_EXPIRES ?? "15m",
+    algorithm: process.env.JWT_ALGORITHM ?? "HS384",
+  });
+
+module.exports.verifyResetPassword = (token) =>
+  jwt.verify(token, process.env.JWT_RESET_PASSWORD_KEY ?? "key");
